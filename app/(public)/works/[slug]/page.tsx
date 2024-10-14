ee27336a3ff7a42242c823ad530f9f1c1902f3d4
Eun Oh <bdohhhhh@gmail.com>
2024-10-14 14:25:51 +0900

    [chore]

--- app/(public)/works/[slug]/page.tsx
+++ app/(public)/works/[slug]/page.tsx
@@ -1,21 +1,21 @@
-import { basicMeta } from "@/app/basicMeta";
-import Loading from "@/app/loading";
-import { years_data } from "@/data/yearsData";
-import { Year } from "@/types/data.type";
-import { Suspense } from "react";
-import EveryWorks from "./_components/EveryWorks";
+import { basicMeta } from '@/app/basicMeta';
+import Loading from '@/app/loading';
+import { years_data } from '@/data/yearsData';
+import { Year } from '@/types/data.type';
+import { Suspense } from 'react';
+import EveryWorks from './_components/EveryWorks';
 
 export const metadata = basicMeta;
 
 export default async function Page({ params }: { params: { slug: string } }) {
-    const { slug } = params;
-    const year: Year = years_data.find((e) => e.year === slug)!;
+  const { slug } = params;
+  const year: Year = years_data.find((e) => e.year === slug)!;
 
-    if (slug.length > 0 && year) {
-        return (
-            <Suspense fallback={<Loading />}>
-                <EveryWorks year={year.year} />
-            </Suspense>
-        );
-    }
+  if (slug.length > 0 && year) {
+    return (
+      <Suspense fallback={<Loading />}>
+        <EveryWorks year={year.year} />
+      </Suspense>
+    );
+  }
 }

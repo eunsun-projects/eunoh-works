73d5e168e7f502a78b9ebeb6339499d425d339be
EunOh <bdohhhhh@gmail.com>
2025-07-13 22:22:59 +0900

    [refactor]

--- app/api/glbs/sonny/route.ts
+++ app/api/glbs/sonny/route.ts
@@ -1,16 +1,16 @@
-import { promises as fs } from "fs";
-import { join } from "path";
+import { promises as fs } from 'node:fs';
+import { join } from 'node:path';
 export async function GET() {
-    const filePath = join(process.cwd(), "public", "assets", "models", "sonny_high.glb");
-    // 파일 읽기
-    const fileBuffer = await fs.readFile(filePath);
+  const filePath = join(process.cwd(), 'public', 'assets', 'models', 'sonny_high.glb');
+  // 파일 읽기
+  const fileBuffer = await fs.readFile(filePath);
 
-    // 응답 반환
-    return new Response(fileBuffer, {
-        status: 200,
-        headers: {
-            "Content-Type": "application/octet-stream",
-            "Content-Length": fileBuffer.length.toString(),
-        },
-    });
+  // 응답 반환
+  return new Response(fileBuffer, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Length': fileBuffer.length.toString(),
+    },
+  });
 }
